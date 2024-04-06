19e331ccabca3a5ae39491b29b540ddbe8fe21c6
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-04-06 14:52:07 +0100

    link width

--- src/components/mobileSideBar.jsx
+++ src/components/mobileSideBar.jsx
@@ -18,7 +18,7 @@ function MobileSideBar({ isOpen, onClose }) {
           <img src={Logo} alt="Logo" className="h-[20px] md:h-[34px]" />
         </div>
         <p className="mt-2">Howdy Folaranmi,</p>
-        <div className="w-11/12 mx-auto mt-14">
+        <div className="w-full mx-auto mt-14">
           {sidebarLinks.map((item) => {
             const isActive =
               pathname === item.route || pathname.startsWith(`${item.route}/`);

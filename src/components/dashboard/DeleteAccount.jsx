4a406977dad225b216589ef60fe19c6ab0c54d4d
folaranmi <olujobifolaranmi@gmail.com>
2024-04-14 16:42:43 +0100

    delete account done

--- src/components/dashboard/DeleteAccount.jsx
+++ src/components/dashboard/DeleteAccount.jsx
@@ -4,17 +4,37 @@ import { CustomModal } from "../CustomModal";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useDeleteUserMutation } from "../../features/api/users";
+import { useSelector } from "react-redux";
+import { toastError, toastSuccess } from "../Toast";
+import { Loader2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { userLogout } from "../../features/auth/authSliceUser";
+
+
 
 const DeleteAccount = ({ isOpen, onClose, setIsOpen }) => {
   const { userInfo } = useSelector((state) => state.authUser);
   const id = userInfo?._id;
-  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+
   const [deleteUser, { isLoading }] = useDeleteUserMutation();
 
   const onUserDelete = () => {
-    deleteUser(id).unwrap().then((res) => {
-      toastSuccess("Account  Deleted")
-    });
+    try {
+      deleteUser(id).unwrap().then((res) => {
+        toastSuccess("Account Deleted successfully.");
+        setIsOpen(!isOpen);
+        localStorage.removeItem("userInfo");
+        localStorage.removeItem("usertoken");
+        dispatch(userLogout());
+        navigate("/login");
+      });
+    } catch (error) {
+      toastError("Something went wrong while deleting your account.");
+    }
   }
 
   return (
@@ -29,7 +49,14 @@ const DeleteAccount = ({ isOpen, onClose, setIsOpen }) => {
         and remove your data from our servers.
       </p>
       <div className="flex justify-end">
-        <Button variant="destructive" onClick={() => setIsOpen(!isOpen)} >Yes, delete account</Button>
+        <Button
+          variant="destructive"
+          onClick={onUserDelete}
+          disabled={isLoading}
+        >
+          {!isLoading && "Yes, delete account"}
+          {isLoading && <>Deleting account <Loader2 className="w-5 h-5 ml-2 animate-spin" /></>}
+        </Button>
       </div>
     </CustomModal>
   );

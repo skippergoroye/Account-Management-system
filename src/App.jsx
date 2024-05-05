812c198b881a426e9cc9481ad7dadf07007c599e
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-05 13:23:13 +0100

    fix: semicolon error

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ const router = createBrowserRouter(
 function App() {
   return (
     <>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <ToastContainer />
     </>
   );
